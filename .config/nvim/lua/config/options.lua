a6cf745ed06b15931ea1d1174d19d8d7eb56dcf7
John Bruckler <john.bruckler@dtmidstream.com>
2025-02-24 13:08:27 -0500

    update options.lua to use variable to declare path to nvim.pssc

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -26,44 +26,33 @@ vim.schedule(function()
   vim.opt.clipboard = 'unnamedplus'
 end)
 
-opt.breakindent  = true  -- Enable break indent
-opt.undofile     = true  -- Save undo history
+opt.breakindent  = true -- Enable break indent
+opt.undofile     = true -- Save undo history
 
-opt.ignorecase   = true  -- case inSenSItIve searching unless \C or
-opt.smartcase    = true  -- one or more capital letters in search term
+opt.ignorecase   = true -- case inSenSItIve searching unless \C or
+opt.smartcase    = true -- one or more capital letters in search term
 
 opt.signcolumn   = 'yes' -- Keep signcolumn on by default
 
-opt.updatetime   = 250   -- Decrease update time
-opt.timeoutlen   = 300   -- Decrease mapped sequence wait time
+opt.updatetime   = 250  -- Decrease update time
+opt.timeoutlen   = 300  -- Decrease mapped sequence wait time
 
-opt.splitright   = true  -- configure how splits should be opened
+opt.splitright   = true -- configure how splits should be opened
 opt.splitbelow   = true
 
 opt.list         = true -- sets how neovim will display certain whitespace
-opt.listchars    = {    -- see `:help list` and `:help 'listchars'`
+opt.listchars    = {   -- see `:help list` and `:help 'listchars'`
   tab = '» ',
   trail = '·',
   nbsp = '␣'
 }
 
--- opt.inccommand   = 'split' -- Preview substitutions live, as you type!
--- opt.cursorline   = true    -- show which line the cursor is on
--- opt.scrolloff    = 10      -- min # of lines to keep above and below the cursor
-
--- opt.shell        = "pwsh"
--- opt.shellcmdflag =
--- "-NoLogo -ExecutionPolicy RemoteSigned -ConfigurationFile C:\\Users\\u55398\\.config\\nvim\\nvim.pssc -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.ASCIIEncoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
--- opt.shellquote   = ''
--- opt.shellxquote  = ''
--- opt.shellredir   = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
--- opt.shellpipe    = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
-
+local homePath   = vim.env.HOME
 opt.shell        = "pwsh"
 opt.shellcmdflag = table.concat({
   '-NoLogo',
   '-NonInteractive',
-  '-ConfigurationFile "C:/ProgramData/config/nvim.pssc"',
+  '-ConfigurationFile "' .. homePath .. '/.config/nvim/nvim.pssc"',
   '-ExecutionPolicy RemoteSigned',
   '-Command'
 }, ' ')
