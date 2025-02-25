f9ccf394b9be74bf3a5ed088f3746a47fb660edd
John Bruckler <jpbruckler@gmail.com>
2025-02-25 00:14:28 -0500

    watching nvim setup videos is bad for your health

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,31 @@
+local opts = { noremap = true, silent = true }
+
+-- Save file
+vim.keymap.set('n', '<C-s>', '<cmd>w<CR>', { desc = 'Save with CTRL + S' })
+
+-- save file without auto-formatting
+vim.keymap.set('n', '<leader>sn', '<cmd>noautocmd w <CR>', opts)
+
+-- quit file
+vim.keymap.set('n', '<C-q>', '<cmd> q <CR>', opts)
+
+-- delete single character without copying into register
+vim.keymap.set('n', 'x', '"_x', opts)
+
+-- Vertical scroll and center
+vim.keymap.set('n', '<C-d>', '<C-d>zz', opts)
+vim.keymap.set('n', '<C-u>', '<C-u>zz', opts)
+
+-- Find and center
+vim.keymap.set('n', 'n', 'nzzzv', opts)
+vim.keymap.set('n', 'N', 'Nzzzv', opts)
+
+-- Resize with arrows
+vim.keymap.set('n', '<Up>', ':resize -2<CR>', opts)
+vim.keymap.set('n', '<Down>', ':resize +2<CR>', opts)
+vim.keymap.set('n', '<Left>', ':vertical resize -2<CR>', opts)
+vim.keymap.set('n', '<Right>', ':vertical resize +2<CR>', opts)
+
 -- See :help hlsearch
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear highlights when pressing <esc> in normal mode." })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix' })
@@ -7,15 +35,49 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = 'Open diagn
 -- Use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+
+-- Buffers
+vim.keymap.set('n', '<Tab>', ':bnext<CR>', opts)
+vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', opts)
+vim.keymap.set('n', '<leader>x', ':bdelete!<CR>', opts)   -- close buffer
+vim.keymap.set('n', '<leader>b', '<cmd> enew <CR>', opts) -- new buffer
+
+-- Window management
+vim.keymap.set('n', '<leader>v', '<C-w>v', opts)      -- split window vertically
+vim.keymap.set('n', '<leader>h', '<C-w>s', opts)      -- split window horizontally
+vim.keymap.set('n', '<leader>se', '<C-w>=', opts)     -- make split windows equal width & height
+vim.keymap.set('n', '<leader>xs', ':close<CR>', opts) -- close current split window
+
+-- Tabs
+vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts)   -- open new tab
+vim.keymap.set('n', '<leader>tx', ':tabclose<CR>', opts) -- close current tab
+vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts)     --  go to next tab
+vim.keymap.set('n', '<leader>tp', ':tabp<CR>', opts)     --  go to previous tab
+
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Toggle line wrapping
+vim.keymap.set('n', '<leader>lw', '<cmd>set wrap!<CR>', opts)
+
+-- Stay in indent mode
+vim.keymap.set('v', '<', '<gv', opts)
+vim.keymap.set('v', '>', '>gv', opts)
+
+-- Keep last yanked when pasting
+vim.keymap.set('v', 'p', '"_dP', opts)
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+-- vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+-- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+-- File picker and grep open files
 vim.keymap.set("n", "<C-T>", ":Pick files<CR>")
 vim.keymap.set("n", "<C-P>", ":Pick grep_live<CR>")
 
@@ -23,7 +85,7 @@ vim.keymap.set("n", "<C-P>", ":Pick grep_live<CR>")
 vim.keymap.set("n", "<BS>", ":lua MiniTrailspace.trim()<CR>")
 
 local minifiles_toggle = function(...)
-  if not MiniFiles.close() then MiniFiles.open(...) end
+    if not MiniFiles.close() then MiniFiles.open(...) end
 end
 vim.keymap.set("n", "-", minifiles_toggle, { desc = 'Toggle MiniFiles' })
 
@@ -43,35 +105,35 @@ vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find exis
 
 -- Slightly advanced example of overriding default behavior and theme
 vim.keymap.set('n', '<leader>/', function()
-  -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-  builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
+    -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+    builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+        winblend = 10,
+        previewer = false,
+    })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
 -- It's also possible to pass additional configuration options.
 --  See `:help telescope.builtin.live_grep()` for information about particular keys
 vim.keymap.set('n', '<leader>s/', function()
-  builtin.live_grep {
-    grep_open_files = true,
-    prompt_title = 'Live Grep in Open Files',
-  }
+    builtin.live_grep {
+        grep_open_files = true,
+        prompt_title = 'Live Grep in Open Files',
+    }
 end, { desc = '[S]earch [/] in Open Files' })
 
 -- Shortcut for searching your Neovim configuration files
 vim.keymap.set('n', '<leader>sn', function()
-  builtin.find_files { cwd = vim.fn.stdpath 'config' }
+    builtin.find_files { cwd = vim.fn.stdpath 'config' }
 end, { desc = '[S]earch [N]eovim files' })
 vim.keymap.set("n", "<leader>en", function()
-  require('telescope.builtin').find_files {
-    cwd = vim.fn.stdpath("config")
-  }
+    require('telescope.builtin').find_files {
+        cwd = vim.fn.stdpath("config")
+    }
 end)
 vim.keymap.set("n", "<leader>ep", function()
-  require('telescope.builtin').find_files {
-    cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
-  }
+    require('telescope.builtin').find_files {
+        cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
+    }
 end)
 
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,64 +1,66 @@
-local opt = vim.opt
+vim.g.have_nerd_font = true -- for other settings later
 
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+-- :help vim.opt
+-- :help option-list
 
-vim.g.mapleader = ' '      -- <space>, both the final frontier and the leader key
-vim.g.maplocalleader = ' ' -- see `:help mapleader`
-vim.g.have_nerd_font = true
+vim.wo.number        = true                    -- turn line numbers on
+vim.o.relativenumber = true                    -- turn on relaative line numbers
+vim.o.clipboard      = 'unnamedplus'           -- sync os <--> vim clipboard
+vim.o.wrap           = false                   -- do not word wrap
+vim.o.linebreak      = true                    -- with wrap, don't split words
+vim.o.mouse          = 'a'                     -- enable mouse mode
+vim.o.autoindent     = true                    -- copy indent from current line
+vim.o.ignorecase     = true                    -- case-insensitive searching UNLESS \C or capital in search
+vim.o.smartcase      = true                    -- enable smart case
+vim.o.shiftwidth     = 4                       -- number of spaces inserted for an indent (default 8)
+vim.o.tabstop        = 4                       -- insert N spaces for a tab
+vim.o.softtabstop    = 4                       -- number of spaces a tab counts for
+vim.o.expandtab      = true                    -- uses spaces instead of tabs
+vim.o.scrolloff      = 4                       -- min # of screen lines to keep above/below cursor
+vim.o.sidescrolloff  = 8                       -- min # of screen columns on either side of cursor if wrap is `false`
+vim.o.showmode       = false                   -- don't show the mode since it's already in the status line
+vim.o.breakindent    = true                    -- Enable break indent
+vim.o.undofile       = true                    -- Save undo history
+vim.o.ignorecase     = true                    -- case inSenSItIve searching unless \C or
+vim.o.smartcase      = true                    -- one or more capital letters in search term
+vim.o.signcolumn     = 'yes'                   -- Keep signcolumn on by default
+vim.o.updatetime     = 250                     -- Decrease update time
+vim.o.timeoutlen     = 300                     -- Decrease mapped sequence wait time
+vim.o.splitright     = true                    -- force vertifal splits to the right of the current window
+vim.o.splitbelow     = true                    -- force horizontal splits below current window
+vim.o.cursorline     = false                   -- do not highlight
+vim.o.numberwidth    = 2
+vim.o.list           = true                    -- sets how neovim will display certain whitespace
+vim.o.updatetime     = 250                     -- Decrease update time (default: 4000)
+vim.o.timeoutlen     = 300                     -- Time to wait for a mapped sequence to complete (in milliseconds) (default: 1000)
+vim.o.backup         = false                   -- Creates a backup file (default: false)
+vim.o.writebackup    = false                   -- If a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited (default: true)
+vim.o.undofile       = true                    -- Save undo history (default: false)
+vim.o.completeopt    =
+'menuone,noselect'                             -- Set completeopt to have a better completion experience (default: 'menu,preview')
+vim.opt.shortmess:append 'c'                   -- Don't give |ins-completion-menu| messages (default: does not include 'c')
+vim.opt.iskeyword:append '-'                   -- Hyphenated words recognized by searches (default: does not include '-')
+vim.opt.formatoptions:remove { 'c', 'r', 'o' } -- Don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode. (default: 'croql')
 
--- [[ set options ]]
--- see `:help vim.opt`
--- see `:help option-list`
--- make line numbers default
-opt.number = true
--- vim.opt.relativenumber = true
-
-opt.mouse = 'a'      -- enable mouse mode (helps with resizing splits)
-opt.showmode = false -- don't show the mode since it's already in the status line
-
--- sync clipboard OS <--> Neovim
--- see `:help clipboard`
--- Schedule this after `UiEnter` to minimize startup time impact
-vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
-end)
-
-opt.breakindent  = true -- Enable break indent
-opt.undofile     = true -- Save undo history
-
-opt.ignorecase   = true -- case inSenSItIve searching unless \C or
-opt.smartcase    = true -- one or more capital letters in search term
-
-opt.signcolumn   = 'yes' -- Keep signcolumn on by default
-
-opt.updatetime   = 250  -- Decrease update time
-opt.timeoutlen   = 300  -- Decrease mapped sequence wait time
-
-opt.splitright   = true -- configure how splits should be opened
-opt.splitbelow   = true
-
-opt.list         = true -- sets how neovim will display certain whitespace
-opt.listchars    = {   -- see `:help list` and `:help 'listchars'`
-  tab = '» ',
-  trail = '·',
-  nbsp = '␣'
+vim.opt.listchars = {                          -- see `:help list` and `:help 'listchars'`
+    tab = '» ',
+    trail = '·',
+    nbsp = '␣'
 }
 
-if package.config:sub(1,1) == '\\' then
-  local homePath   = vim.env.HOME
-  opt.shell        = "pwsh"
-  opt.shellcmdflag = table.concat({
-    '-NoLogo',
-    '-NonInteractive',
-    '-ConfigurationFile "' .. homePath .. '/.config/nvim/nvim.pssc"',
-    '-ExecutionPolicy RemoteSigned',
-    '-Command'
-  }, ' ')
-  opt.shellquote   = ''
-  opt.shellxquote  = ''
-  opt.shellredir   = '2>&1 | Out-File -Encoding UTF8 %s'
-  opt.shellpipe    = '2>&1 | Out-File -Encoding UTF8 %s'
+-- In Windows, set the terminal shell to PowerShell
+if package.config:sub(1, 1) == '\\' then
+    local homePath     = vim.env.HOME
+    vim.o.shell        = "pwsh"
+    vim.o.shellcmdflag = table.concat({
+        '-NoLogo',
+        '-NonInteractive',
+        '-ConfigurationFile "' .. homePath .. '/.config/nvim/nvim.pssc"',
+        '-ExecutionPolicy RemoteSigned',
+        '-Command'
+    }, ' ')
+    vim.o.shellquote   = ''
+    vim.o.shellxquote  = ''
+    vim.o.shellredir   = '2>&1 | Out-File -Encoding UTF8 %s'
+    vim.o.shellpipe    = '2>&1 | Out-File -Encoding UTF8 %s'
 end
