409c7bba2815e15b5617e68be93089054ff0b224
jpbruckler <jpbruckler@gmail.com>
2025-03-28 13:50:50 -0400

    fix: replace fzf-lua.lua

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -1,35 +1,67 @@
 return {
-  'saghen/blink.cmp',
-  dependencies = { 'rafamadriz/friendly-snippets' },
-  version = '*',
-  build = 'cargo build --release',
-  opts = {
-    keymap = { preset = 'default' },
-    appearance = {
-      nerd_font_variant = 'mono'
-    },
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
-    fuzzy = {
-      implementation = 'prefer_rust_with_warning',
-      max_typos = function(keyword) return math.floor(#keyword / 4) end,
-      use_frecency = true,
-      use_proximity = true,
-      use_unsafe_no_lock = false,
-      sorts = {
-        'score',
-        'sort_text',
-      },
-
-      prebuilt_binaries = {
-        download = false,
-        ignore_version_mismatch = false,
-        force_version = nil,
-        force_system_triple = nil,
-        extra_curl_args = {}
-      },
-    },
+  {
+    "saghen/blink.compat",
+    -- use the latest release, via version = '*', if you also use the latest release for blink.cmp
+    version = "*",
+    -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
+    lazy = true,
+    -- make sure to set opts so that lazy.nvim calls blink.compat's setup
+    opts = {},
   },
-  opts_extend = { "sources.default" },
+  {
+    "saghen/blink.cmp",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      "moyiz/blink-emoji.nvim",
+    },
+    version = "1.*",
+    build = "cargo build --release",
+    opts = {
+      keymap = { preset = "default" },
+      appearance = {
+        nerd_font_variant = "mono",
+      },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer", "emoji", "cmdline", "omni" },
+        providers = {
+          emoji = {
+            module = "blink-emoji",
+            name = "Emoji",
+            score_offset = 15,        -- Tune by preference
+            opts = { insert = true }, -- Insert emoji (default) or complete its name
+            should_show_items = function()
+              return vim.tbl_contains(
+              -- Enable emoji completion only for git commits and markdown.
+              -- By default, enabled for all file-types.
+                { "gitcommit", "markdown" },
+                vim.o.filetype
+              )
+            end,
+          },
+        },
+      },
+      fuzzy = {
+        implementation = "prefer_rust_with_warning",
+        max_typos = function(keyword)
+          return math.floor(#keyword / 4)
+        end,
+        use_frecency = true,
+        use_proximity = true,
+        use_unsafe_no_lock = false,
+        sorts = {
+          "score",
+          "sort_text",
+        },
+
+        prebuilt_binaries = {
+          download = false,
+          ignore_version_mismatch = false,
+          force_version = nil,
+          force_system_triple = nil,
+          extra_curl_args = {},
+        },
+      },
+    },
+    opts_extend = { "sources.default" },
+  },
 }
