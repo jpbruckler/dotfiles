2faf464337677d3a120a9c6cbf62ec7b3048b230
John Bruckler <jpbruckler@gmail.com>
2025-06-19 10:17:47 -0400

    Lazy lock updates and formatting tomfoolery for powershell_es

--- nvim/.config/nvim/lua/plugins/lspconfig/servers.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig/servers.lua
@@ -34,32 +34,32 @@ return {
           -- nvim-data/mason/packages/powershell-editor-services/mason-schemas/lsp.json
           -- Setting `preset` to anything other than Custom will override all other format
           -- settings.
-          preset = "Custom",                                                -- One of Custom, Allman, OTBS, Stroustrup
-          addWhitespaceAroundPipe = true,                                   -- Defaults to true
-          avoidSemicolonsAsLineTerminators = true,                          -- Defaults to false
-          trimWhitespaceAroundPipe = false,                                 -- Defaults to false
-          openBraceOnSameLine = true,                                       -- Defaults to true
-          ignoreOneLineBlock = true,                                        -- Defaults to true
-          newLineAfterOpenBrace = true,                                     -- Defaults to true
-          alignPropertyValuePairs = true,                                   -- Defaults to true
-          newLineAfterCloseBrace = true,                                    -- Defaults to true
-          useConstantStrings = true,                                        -- Defaults to true
+          preset = "Custom",                                           -- One of Custom, Allman, OTBS, Stroustrup
+          addWhitespaceAroundPipe = true,                              -- Defaults to true
+          avoidSemicolonsAsLineTerminators = true,                     -- Defaults to false
+          trimWhitespaceAroundPipe = false,                            -- Defaults to false
+          openBraceOnSameLine = true,                                  -- Defaults to true
+          ignoreOneLineBlock = true,                                   -- Defaults to true
+          newLineAfterOpenBrace = true,                                -- Defaults to true
+          alignPropertyValuePairs = true,                              -- Defaults to true
+          newLineAfterCloseBrace = true,                               -- Defaults to true
+          useConstantStrings = true,                                   -- Defaults to true
           pipelineIndentationStyle = "IncreaseIndentationForFirstPipeline", -- Defaults to NoIndentation
-          useCorrectCasing = true,                                          -- Defaults to false
-          whitespaceBeforeOpenBrace = true,                                 -- Defaults to true
-          whitespaceBeforeOpenParen = true,                                 -- Defaults to true
-          whitespaceAfterSeparator = true,                                  -- Defaults to true
-          whitespaceInsideBrace = true,                                     -- Defaults to true
-          whitespaceBetweenParameters = true,                               -- Defaults to false
-          whitespaceAroundOperator = true,                                  -- Defaults to true
-          autoCorrectAliases = true,                                        -- Defaults to false
+          useCorrectCasing = true,                                     -- Defaults to false
+          whitespaceBeforeOpenBrace = true,                            -- Defaults to true
+          whitespaceBeforeOpenParen = true,                            -- Defaults to true
+          whitespaceAfterSeparator = true,                             -- Defaults to true
+          whitespaceInsideBrace = true,                                -- Defaults to true
+          whitespaceBetweenParameters = true,                          -- Defaults to false
+          whitespaceAroundOperator = true,                             -- Defaults to true
+          autoCorrectAliases = true,                                   -- Defaults to false
         },
         scriptAnalysis = {
           enable = true,
         },
-        developer = {
-          editorServicesLogLevel = "Information",
-        },
+        --        developer = {
+        --        editorServicesLogLevel = "Information",
+        --        },
       },
     },
     init_options = {
